735519cb5544e557c3e8790c32e3575daa0e86d7
phil-schreiber <schreiber@denkfabrik-group.com>
2015-03-20 13:08:20 +0100

    latest

--- public/js/vendor/mailobjectsUpdate.js
+++ public/js/vendor/mailobjectsUpdate.js
@@ -83,11 +83,11 @@ var pollForTinymce=function(){
 			plugins: [
 				"customem advlist autolink lists link image charmap print preview anchor",
 				"searchreplace visualblocks code fullscreen",
-				"insertdatetime media table contextmenu paste"
+				"insertdatetime media table contextmenu paste jbimages"
 			],
 			extended_valid_elements : "dynamic",
 			custom_elements: "~dynamic",			
-			toolbar: "customem | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code"
+			toolbar: "customem | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages | code"
 		});
 		
 		
